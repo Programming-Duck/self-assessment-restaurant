047f71a3a53219aead6ab9957880d0948bebf5f0
LouiChew <louisbhchew@gmail.com>
2022-01-29 16:57:38 -0800

    isolated only the previous button in javascript

---
+++ contact-us.js
@@ -0,0 +1,6 @@
+function validateEmail() {
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!document.getElementByID("#Email").value.match(emailRegex)) {
+        document.querySelector('button[type="submit"]').innerHTML("")
+    }
+}
--- script.js
+++ script.js
@@ -6,11 +6,13 @@ function modulo(number, mod) {
     return result;
 }
 
-class Carousel {
+const buttonPrevious = document.getElementById("data-carousel-button-previous");
+buttonPrevious.addEventListener('click', () => console.log('clicked Previous'));
+/*class Carousel {
     constructor(carousel) {
-        this.buttonPrevious = carousel.querySelector('data-carousel-button-previous');
-        this.buttonNext = carousel.querySelector('data-carousel-button-next');
-        this.slidesContainer = carousel.querySelector('data-carousel-slides-container');
+        this.buttonPrevious = carousel.querySelector(['data-carousel-button-previous']);
+        this.buttonNext = carousel.querySelector(['data-carousel-button-next']);
+        this.slidesContainer = carousel.querySelector(['data-carousel-slides-container']);
         this.currentSlide = 0;
         this.numSlides = this.slidesContainer.children.length;
 
@@ -30,4 +32,4 @@ class Carousel {
 }
 
 const carousels = document.querySelectorAll("[data-carousel]");
-carousels.forEach(carouselElement => new Carousel(carouselElement));
+carousels.forEach(carouselElement => new Carousel(carouselElement));*/
